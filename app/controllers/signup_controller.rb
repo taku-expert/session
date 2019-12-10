e893b3b2ba312701df99e25d8ab60fa00d11eee2
taku-expert <prime.stkvkp@gmail.com>
2019-12-10 21:35:33 +0900

    test

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -10,7 +10,6 @@ class SignupController < ApplicationController
     session[:profile_attributes_after_step1] = user_params[:profile_attributes]  # profileモデルの値をぶっこむ。
     @user = User.new
     @user.build_profile
-    binding.pry
   end
 
   def step3
@@ -18,14 +17,13 @@ class SignupController < ApplicationController
     session[:profile_attributes_after_step2].merge!(session[:profile_attributes_after_step1])  # step2のsessionにstep1のsessionの中身を合わせる。
     @user = User.new
     @user.build_profile
-    binding.pry
-
   end
 
   def create
     @user = User.new(session[:user_params])  # ここでuserモデルのsessionを引数で渡す。
     @user.build_profile(session[:profile_attributes_after_step1])  # ここでprofileモデルのsessionを引数で渡す。
     @user.build_profile(user_params[:profile_attributes])  # 今回のビューで入力された情報を代入。
+    binding.pry
     if @user.save
       binding.pry
       session[:id] = @user.id  #　ここでidをsessionに入れることでログイン状態に持っていける。
@@ -39,8 +37,7 @@ class SignupController < ApplicationController
   private
   def user_params
     params.require(:user).permit(
-      :name,
-      :email,
+      :name, :email,
       profile_attributes: [:id, :family, :number, :city]
     )
   end
