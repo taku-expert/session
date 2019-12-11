2dd1a26d1c53204c2ded5897bd173cdea5a25c95
taku-expert <prime.stkvkp@gmail.com>
2019-12-12 03:09:29 +0900

    sessionメソッド使用の完成

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -8,11 +8,10 @@ class SignupController < ApplicationController
   def step2
     @user = User.new(user_params)
     @profile = Profile.new(user_profile_params)
-    if @user.save! && @profile.save!
+    if @user.valid? && @profile.valid?
       session[:email] = user_params[:email]
       session[:name] = user_params[:name]
-      session[:family] = profile_params[:family]
-      render 'signup/step3'
+      session[:family] = user_profile_params[:family]
     else
       redirect_to action: 'step1'
     end
@@ -21,18 +20,40 @@ class SignupController < ApplicationController
   def step3
     @profile = Profile.new(profile_params)
     if @profile.valid?
-    session[:number] = profile_params[:number]
+      session[:number] = profile_params[:number]
     else
-      redirect_to action: 'step2'
+      redirect_to action: 'step1'
     end
   end
 
   def create
     @profile = Profile.new(profile_params)
-    @user = User.new(
-      name: session[:name]
-    )
+    session[:city] = profile_params[:city]
+    if @profile.valid?
+      @user = User.new(
+        name: session[:name],
+        email: session[:email],
+        password: 111111,
+        password_confirmation: 111111
+      )
+      @profile = Profile.new(
+        family: session[:family],
+        number: session[:number],
+        city: session[:city]
+      )
+      @user.save!
+      @profile.save!
+      redirect_to action: 'index'
+    else
+      redirect_to action: 'step1'
+    end
   end
+
+  def index
+    @users = User.all
+    @profiles = Profile.all
+  end
+
 
   private
 
